1e7bd510460195dce4d4d89810c6edffeac12611
Junior Samaroo <jns0987654321@gmail.com>
2017-02-25 23:16:15 -0500

    Initial Commit

---
+++ mvc-test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mvc-test
+//
+//  Created by Junior Samaroo on 2017-02-25.
+//  Copyright © 2017 Junior Samaroo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
